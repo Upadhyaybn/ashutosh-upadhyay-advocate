3b529c442f560e506fc51c6678d95f873b4b44c3
Bhawani Nandan Upadhyay <upadhyaybn321@gmail.com>
2026-08-16 18:31:27 +0530

    chore: configure PostgreSQL datasource and test profile

--- backend/src/test/java/com/ashutoshupadhyay/advocate/AdvocateApiApplicationTests.java
+++ backend/src/test/java/com/ashutoshupadhyay/advocate/AdvocateApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.ashutoshupadhyay.advocate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AdvocateApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
